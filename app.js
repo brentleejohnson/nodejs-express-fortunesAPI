bf570b23e8b94e821735bd2ab2891b11e8c4d4ab
brentleejohnson <brentleejohnson73@gmail.com>
2021-09-29 09:58:01 +0200

    Added UPDATE

--- app.js
+++ app.js
@@ -18,6 +18,14 @@ app.get("/fortunes/:id", (req, res) => {
   res.json(fortunes.find((f) => f.id == req.params.id));
 });
 
+const writeFortunes = (json) => {
+  fs.writeFile(
+    "./data/fortunes.json",
+    JSON.stringify(json, (err) => console.log(err))
+  );
+};
+
+// POST METHOD
 app.post("/fortunes", (req, res) => {
   console.log(req.body);
 
@@ -32,12 +40,27 @@ app.post("/fortunes", (req, res) => {
     spirit_animal,
   });
 
-  fs.writeFile(
-    "./data/fortunes.json",
-    JSON.stringify(new_fortunes, (err) => console.log(err)) // If error, arrow notation should be '='
-  );
+  writeFortunes(new_fortunes);
 
   res.json(new_fortunes);
+});
+
+// UPDATE
+app.put("/fortunes/:id", (req, res) => {
+  const { id } = req.params;
+  const { message, lucky_number, spirit_animal } = req.body;
+
+  const old_fortune = fortunes.find((f) => f.id == id);
+
+  if (message) old_fortune.message = message;
+  if (lucky_number) old_fortune.lucky_number = lucky_number;
+  if (spirit_animal) old_fortune.spirit_animal = spirit_animal;
+
+  fs.writeFile("./data/fortunes.json", JSON.stringify(fortunes), (err) =>
+    console.log(err)
+  );
+
+  res.json(fortunes);
 });
 
 module.exports = app;
